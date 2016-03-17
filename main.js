c22a0342014448f9b10d81a1738d385117ef1b96
Damjan <dpavlica@itekako.com>
2016-03-17 19:16:51 +0100

    template example

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 'use strict';
+/*
+  moze da prevodi sablone iz html-a i iz js-a
+  kada vuce iz htmla, mora negde sacuvati izvornu verziju, da bi mogao da je evaluira svaki put
+  kad prevodi iz js-a, mora svaki put pripisivati varijablu kako bi je azurirao (nece sa eval kao iz htmla; mozda da je vraca iz funkcije?)
+*/
 
 let recnik = {
   ime: 'Daman',
@@ -9,18 +14,10 @@ let recnik = {
 
 var tabela;
 var tabela2;
+var tabelaSablon2;
 
-window.onload = function functionName() {
-  tabela = document.createElement('div');
-  document.body.appendChild(tabela);
-  tabela2 = document.getElementById('tabela2')
-}
-
-
-function update() {
-  recnik.energija++
-
-  let statistike = `
+function tabelaSablon3() {
+  return `
   <h3>Tabela</h2>
   <div>
     Životi: ${recnik.zivoti}<br>
@@ -29,14 +26,33 @@ function update() {
     ${recnik.poeni} je ${ (recnik.poeni % 2 === 0 ? "par" : "nepar") }
   </div>
   `
-  tabela.innerHTML = statistike;
+}
 
-  let parsirano = eval('`' + tabela2.innerHTML + '`');
-  console.log(tabela2.innerHTML);
-  tabela2.innerHTML = parsirano;
+window.onload = function init() {
+  tabela = document.createElement('div');
+  document.body.appendChild(tabela);
+  tabela2 = document.getElementById('tabela2')
+  tabelaSablon2 = tabela2.innerHTML;
+} // init
+
+
+function update() {
+  recnik.energija++
+
+  let tabelaSablon = `
+  <h3>Tabela</h2>
+  <div>
+    Životi: ${recnik.zivoti}<br>
+    Poeni: ${recnik.poeni}<br>
+    Energija: ${recnik.energija}<br>
+    ${recnik.poeni} je ${ (recnik.poeni % 2 === 0 ? "par" : "nepar") }
+  </div>
+  `
+  tabela.innerHTML = tabelaSablon;
+
+  let parsirano2 = eval('`' + tabelaSablon2 + '`');
+  tabela2.innerHTML = parsirano2;
 } // update
 
-
-// document.body.innerHTML += statistike;
 
 setInterval(update, 1000)
