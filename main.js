c78f777e58ecc9edeb020ad54a63e4d77317bbfd
mudroljub <mudroljub@gmail.com>
2016-03-22 00:21:21 +0100

    pomalo

--- main.js
+++ main.js
@@ -3,24 +3,44 @@
   kada vuce iz htmla: ucitava sadrzaj, cuva izvornu verziju, i evaluira svaki put
   kad prevodi iz js-a: pravi sablon, koji mora vracati iz funkcije
 
-  problem: kad zadatak nije definisan puca sve (ako je prazan sve kul)
   razmisliti da ipak sve bude u posebnim html stranicama?
 */
 
 /*** KONFIG ***/
 
-// dodati sceni
-var naslov = "Bitka za Krupanj";
-var zadatak = "Dovedi Žikicu do nemačkog bunkera.";
 var tabela;
 var zaglavlje;
 
+let scena = {
+  naslov: "Bitka za Krupanj",
+  zadatak: "Dovedi Žikicu do nemačkog bunkera."
+}
 let statistike = {
   ime: 'Daman',
   zivoti: 2,
   poeni: 222,
   energija: 100
 }
+
+/*** LOGIKA ***/
+
+window.onload = init;
+setInterval(update, 1000)
+
+function init() {
+  zaglavlje = document.getElementById('zaglavlje');
+  zaglavlje.innerHTML = eval('`' + zaglavlje.innerHTML + '`');
+  tabela = document.getElementById('tabela') || document.createElement('div');
+  document.body.appendChild(tabela);
+} // init
+
+function update() {
+  statistike.energija++
+  tabela.innerHTML = praviTabelu(statistike);
+} // update
+
+
+/* POMOĆNE FUNKCIJE */
 
 function praviTabelu(statistike) {
   return `
@@ -32,24 +52,3 @@ function praviTabelu(statistike) {
     ${statistike.poeni} je ${ (statistike.poeni % 2 === 0 ? "par" : "nepar") }
   </div>`
 } // praviTabelu
-
-
-/*** LOGIKA ***/
-
-window.onload = init;
-
-function init() {
-  zaglavlje = document.getElementById('zaglavlje');
-  zaglavlje.innerHTML = eval('`' + zaglavlje.innerHTML + '`'); // mora da evaluira
-  tabela = document.getElementById('tabela') || document.createElement('div');
-  document.body.appendChild(tabela);
-} // init
-
-
-function update() {
-  statistike.energija++
-  tabela.innerHTML = praviTabelu(statistike);
-} // update
-
-
-setInterval(update, 1000)
